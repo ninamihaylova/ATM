ce7fc3e2ecf9ba78936a7213cea318a57942bcd2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 23:13:46 +0300

    Initial commit

---
+++ ATM/Program.cs
@@ -0,0 +1,24 @@
+﻿class ATM
+{
+    public static void Main(String[] args)
+    {
+        int balance = int.Parse(Console.ReadLine());
+        int withdraw = int.Parse(Console.ReadLine());
+        int limit = int.Parse(Console.ReadLine());
+
+        if (withdraw < balance)
+        {
+            Console.WriteLine("The withdraw was successful.");
+        }
+        else if(withdraw>limit) 
+        {
+            Console.WriteLine("The limit was exceeded.");
+
+        }
+        else if(balance<withdraw)
+        {
+            Console.WriteLine("Insufficient availability.");
+            
+        }
+    }
+}
